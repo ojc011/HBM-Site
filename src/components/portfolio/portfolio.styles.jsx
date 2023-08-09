dc9843f775934aba28f51a2b819925cf4903c0e4
Oliver Cronk <ojc011@yahoo.com>
2023-08-09 09:32:06 -0400

    Transformed approvedby y section into slideshow

--- src/components/portfolio/portfolio.styles.jsx
+++ src/components/portfolio/portfolio.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Title = styled.h1`
   font-size: 24px;
   font-weight: bold;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
   color: black; /* Set Title text color to black */
   text-align: center;
   background-color: rgba(0, 0, 0, 0.2);
@@ -19,9 +19,10 @@ export const Container = styled.div`
 `;
 
 export const ParentContainer = styled.div`
-  display: grid;
   gap: 20px;
   padding: 10px 5px; /* Add more horizontal padding for mobile */
+  display: flex;
+  transition: transform 0.3s ease;
 
   @media (min-width: 1024px) {
     grid-template-columns: repeat(
@@ -56,7 +57,8 @@ export const ItemContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   height: 100%;
-
+  min-width: 33.33%; /* Set width to fit three cards */
+  flex: 0 0 auto;
   background-color: rgba(0, 0, 0, 0.2);
   padding: 20px;
   border-radius: 8px;
@@ -90,4 +92,35 @@ export const ItemTitle = styled.h2`
 
 export const ItemDescription = styled.p`
   font-size: 14px;
+`;
+
+export const SlideButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 400px;
+  margin: 20px auto;
+`;
+
+export const SlideButton = styled.button`
+  background-color: #333;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #555;
+  }
+`;
+
+export const SlideCard = styled(ItemContainer)`
+  width: 100%;
+  transition: transform 0.3s ease;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
 `;
---
+++ src/components/portfolio/slideshow.component.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Title,
+  ParentContainer,
+  ItemImage,
+  ItemTitle,
+  ItemDescription,
+  SlideButtonsContainer,
+  SlideButton,
+  SlideCard,
+} from "./portfolio.styles";
+
+const Slideshow = ({ items }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? items.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === items.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  return (
+    <Container>
+      <Title>Approved By</Title>
+      <ParentContainer
+        style={{ transform: `translateX(-${currentIndex * 33.33}%)` }}
+      >
+        {items.map((item, index) => (
+          <SlideCard key={index}>
+            <ItemImage src={item.imageSrc} alt={item.title} />
+            <ItemTitle>{item.title}</ItemTitle>
+            <ItemDescription>{item.description}</ItemDescription>
+          </SlideCard>
+        ))}
+      </ParentContainer>
+      <SlideButtonsContainer>
+        <SlideButton onClick={handlePrevSlide}>Previous</SlideButton>
+        <SlideButton onClick={handleNextSlide}>Next</SlideButton>
+      </SlideButtonsContainer>
+    </Container>
+  );
+};
+
+export default Slideshow;
