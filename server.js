520f83927cecfdda3fc39cdfe3120174a301ac9f
Oliver Cronk <ojc011@yahoo.com>
2024-01-08 11:43:02 -0500

    Forcing https on entire application

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 // Force HTTPS for the entire application
 app.use((req, res, next) => {
     if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(301, `https://${req.hostname}${req.url}`);
+        return res.redirect(301, `https://${req.headers.host}${req.url}`);
     }
     next();
 });
